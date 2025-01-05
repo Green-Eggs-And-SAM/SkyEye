f2eb55e08b325835536cd95e631c21b162b65c22
Green-Eggs-And-SAM <sam.john.alexander@gmail.com>
2025-01-05 13:21:32 -0800

    Unity Build added. Not working

--- sky-eye-react/src/App.js
+++ sky-eye-react/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { unityProvider } = useUnityContext({
+        loaderUrl: "./Game/Build/test1.loader.js",
+        dataUrl: "./Game/Build/test1.data.unityweb",
+        frameworkUrl: "./Game/Build/test1.framework.js.unityweb",
+        codeUrl: "./Game/Build/test1.wasm.unityweb",
+    });
+
+    return (
+        <>
+            <div className="App">
+                <header className="App-header">
+                    <h1>Sky Eye</h1>
+                </header>
+            </div>
+            <Unity unityProvider={unityProvider} />
+        </>
+    );
 }
 
 export default App;
